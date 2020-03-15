6d7afb74e5187cce2483e3f87505825597096f48
Bikram Sodhi <biksodhi@gmail.com>
2020-03-15 18:49:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_box_label = nil
+  smallest_box_size = float::INFINITY
+  name_hash.each do |name, num|
+    if num < smallest_box_label
+      smallest_box_label = name
+      smallest_box_size = num
+    end
+  end
+  return smallest_box_label
 end
