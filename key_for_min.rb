09bba5b7f651a3276f95eacba6ebb4412accd895
Bikram Sodhi <biksodhi@gmail.com>
2020-03-15 18:50:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_box_label = nil
   smallest_box_size = Float::INFINITY
   name_hash.each do |name, num|
-    if num < smallest_box_label
+    if num < smallest_box_size
       smallest_box_label = name
       smallest_box_size = num
     end
